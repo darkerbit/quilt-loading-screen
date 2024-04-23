d9816b5928eeb9f2cae6e1fc4feed4878383b9ed
hibi <69405847+hibiii@users.noreply.github.com>
2024-04-23 15:13:32 -0300

    Add Quilt-colored patches resource pack

--- src/main/java/com/emmacypress/quilt_loading_screen/QLSClientInit.java
+++ src/main/java/com/emmacypress/quilt_loading_screen/QLSClientInit.java
@@ -11,8 +11,12 @@ package com.emmacypress.quilt_loading_screen;
 import com.emmacypress.quilt_loading_screen.mixin.SplashOverlayAccessor;
 import eu.midnightdust.lib.config.MidnightConfig;
 import net.minecraft.client.util.ColorUtil;
+import net.minecraft.text.Text;
+
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import org.quiltmc.qsl.resource.loader.api.ResourceLoader;
+import org.quiltmc.qsl.resource.loader.api.ResourcePackActivationType;
 
 import static com.emmacypress.quilt_loading_screen.QuiltLoadingScreen.MODID;
 
@@ -23,6 +27,8 @@ public class QLSClientInit implements ClientModInitializer {
 	@Override
 	public void onInitializeClient(ModContainer mod) {
 		MidnightConfig.init(MODID, Config.class);
+
+		ResourceLoader.registerBuiltinResourcePack(QuiltLoadingScreen.id("stronger_quilt_branding"), mod, ResourcePackActivationType.NORMAL, Text.translatable("quilt_loading_screen.resource_pack.name"));
 
 		if (Config.modifyBackgroundColor)
 			SplashOverlayAccessor.setMojangRed(ColorUtil.ARGB32.getArgb(0, 35, 22, 56));
